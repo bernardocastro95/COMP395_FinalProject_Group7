78baa3d886d36406f772a4bd01a25f50903e72e4
Bernardo Castro <bernardoc544@gmail.com>
2021-04-03 16:44:03 -0400

    Func

--- NameProjectNA/Assets/Scripts/StartButton.cs
+++ NameProjectNA/Assets/Scripts/StartButton.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartButton : MonoBehaviour
 {
+    Button start;
     // Start is called before the first frame update
     void Start()
     {
-        
+        start = GetComponent<Button>();
+        start.onClick.AddListener(startGame);
     }
 
-    // Update is called once per frame
-    void Update()
+    void startGame()
     {
-        
+        SceneManager.LoadScene("Level1Scene");
     }
 }
