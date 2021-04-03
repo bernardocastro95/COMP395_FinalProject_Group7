3a6bd0632eda00c13d986a7c7f692a1f771dc8fe
Bernardo Castro <bernardoc544@gmail.com>
2021-04-03 15:32:39 -0400

    Menu Scene with Scripts

---
+++ NameProjectNA/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour
+{
+    Button exit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        exit = GetComponent<Button>();
+        exit.onClick.AddListener(exitGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void exitGame()
+    {
+        Debug.Log("Exiting Button has been clicked");
+        Application.Quit();
+    }
+}
