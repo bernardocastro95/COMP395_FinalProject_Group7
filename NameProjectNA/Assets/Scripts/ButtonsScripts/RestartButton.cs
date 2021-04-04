f44160d026c1cb488f8e53614eca6fd5c2a2c207
Bernardo Castro <bernardoc544@gmail.com>
2021-04-04 15:35:28 -0400

    Upgraded Game Over Scene

---
+++ NameProjectNA/Assets/Scripts/ButtonsScripts/RestartButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestartButton : MonoBehaviour
+{
+    Button restart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        restart = GetComponent<Button>();
+        restart.onClick.AddListener(RestartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void RestartGame()
+    {
+        SceneManager.LoadScene("Level1Scene");
+    }
+}
