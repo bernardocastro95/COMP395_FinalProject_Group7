0e11bbf1592593d02bbd9b53b6fb22c6321364e9
Bernardo Castro <bernardoc544@gmail.com>
2021-04-04 18:49:51 -0400

    Level 1 Taking Form

---
+++ NameProjectNA/Assets/Scripts/PlayerScript/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject player;
+    public float playerSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float northSouth = Input.GetAxis("Vertical") * playerSpeed;
+        float westEast = Input.GetAxis("Horizontal") * playerSpeed;
+    }
+}
