4b4c741b4878d6f313d433fa984c14a4f45d13a5
Bernardo Castro <bernardoc544@gmail.com>
2021-04-04 16:16:08 -0400

    Temporary Functionality for Dropdown

---
+++ NameProjectNA/Assets/Scripts/DropDownScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DropDownScript : MonoBehaviour
+{
+    public TextMeshProUGUI output;
+
+    public void handler(int val)
+    {
+        if(val == 0)
+        {
+            output.text = "You chose easy";
+        }
+        else if(val == 1)
+        {
+            output.text = "You chose medium";
+        }
+        else if(val == 2)
+        {
+            output.text = "You chose hard";
+        }
+    }
+}
