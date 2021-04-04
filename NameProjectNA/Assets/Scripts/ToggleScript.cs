75db7c89ae21b3379662e8ab02698d10593b1489
Bernardo Castro <bernardoc544@gmail.com>
2021-04-04 16:29:54 -0400

    Temporary Code for game mutted

---
+++ NameProjectNA/Assets/Scripts/ToggleScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleScript : MonoBehaviour
+{
+    public GameObject toggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (toggle.GetComponent<Toggle>().isOn)
+        {
+            print("Game is muted");
+        }
+        else
+        {
+            print("Game is not muted");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
