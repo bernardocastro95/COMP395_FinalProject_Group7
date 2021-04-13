114abd1e0e292ed7f684a8605da743c19428d563
Bernardo Castro <bernardoc544@gmail.com>
2021-04-13 15:36:32 -0400

    Temporary enemy with script to follow palyer

---
+++ NameProjectNA/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public Transform player;
+    public float speed = 3f;
+    Rigidbody rbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        rbody.MovePosition(position);
+        transform.LookAt(player);
+    }
+}
